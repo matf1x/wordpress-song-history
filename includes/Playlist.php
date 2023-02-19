f35a8b6ba42252cd232d6304e2208430a1786041
Fabian Tack <fabian@matfix.be>
2023-02-19 01:35:34 +0100

    💪 Refactory
    We refactored the whole plugin. The main reason is that strftime is deprecated since PHP 8.1.
    
    We've overhauled the whole plugin to make it more future proof but also more understandable to maintain.

--- includes/Playlist.php
+++ includes/Playlist.php
@@ -10,7 +10,7 @@ if( ! class_exists( 'Playlist' )) {
             'selectedDay' => '',
             'nextDay' => '',
             'shows' => null,
-            'songs' => null
+            'songs' => array()
         );
 
         /**
@@ -19,140 +19,147 @@ if( ! class_exists( 'Playlist' )) {
         public function __construct() {
             
             // Add a shortcode to the Wordpress Library
-            add_shortcode('ra-playlist', array($this, 'loadPlaylistForDay'));
+            add_shortcode('ra-playlist', array($this, 'loadPlaylist'));
 
         }
 
         /**
-         * Create the header to show
+         * Main function for the shortcode
          */
-        private function createHeader() {
+        public function loadPlaylist($atts) {
 
-            // Create a print
-            print('
-            <div class="playlistHeader">
-                <a href="./?date=' . date('Y-m-d', strtotime('- 1 days', strtotime($this->settings['selectedDay']))) . '"><i class="fas fa-angle-left previousBtn"></i></a>
-                <div>' . strftime('%A %e %B %Y', strtotime($this->settings['selectedDay'])) . '</div>
-                <a href="./?date=' . date('Y-m-d', strtotime('+ 1 days', strtotime($this->settings['selectedDay']))) . '"><i class="fas fa-angle-right nextBtn"></i></a>
-            </div>
-
-            <div class="playlistContent">
-            ');
-
-        }
-
-        /**
-         * Load the playlist for the selected date
-         */
-        public function loadPlaylistForDay($atts) {
+            // Show the header
+            $this->showHeader();
 
             // Connect to the database
             $this->connect();
 
-            // Check if attributes are set, otherwise, use the default value
-            $atts = shortcode_atts(
-                array(
-                    'limit' => 25,
-                    'start' => 0
-                ), $atts, 'ra-playlist');
+            // Get the songs based on the date
+            if(!$this->getSongs()) { $this->error(); return; }
 
-            // Get the songs by date
-            $selectSongs = $this->selectSongsByDate();
-
-            // Create the header for the page
-            $this->createHeader();
-
-            // Check if everything worked as planned
-            if(!$selectSongs) {
-                die($this->printError());
-            }
-
-            $this->printSongs($this->songs);
-
-            // Create the closing tag
-            print('
-            </div>
-            ');
-
+            // Show the songs
+            $this->listSongs();
         }
 
         /**
-         * Get the songs for a specific date
+         * Get the songs from the database based on a specific date
          */
-        private function selectSongsByDate() {
+        private function getSongs() {
 
-            // Wrap in a try to chatch all the errors
-            try {
+            // Execute the query
+            try{
 
-                // Set default date
-                $this->settings["selectedDay"] = ($_GET['date']) ? $_GET['date'] : date('Y-m-d');
-
-                // Get the next day
-                $this->settings["nextDay"] = date('Y-m-d', strtotime('+1 day', strtotime($this->settings["selectedDay"])));
-                
-                // Get the songs from a specific date
+                // Create the SQL query
                 $query = 'SELECT songLibrary.artist, songLibrary.title, songLibrary.cover, songHistory.start FROM songHistory INNER JOIN songLibrary ON songHistory.trackGuid = songLibrary.trackGuid WHERE songHistory.start > ? AND songHistory.start < ? ORDER BY songHistory.start ASC';
 
                 // Prepare the query
                 $sth = $this->conn->prepare($query);
 
-                // Execute the query
+                // Execute the created query
                 $sth->execute(array($this->settings["selectedDay"], $this->settings["nextDay"]));
 
-                // Get the songs from the query
-                $this->songs = $sth->fetchAll(PDO::FETCH_ASSOC);
+                // Fetch the songs and put them into the holder
+                $this->settings['songs'] = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-                // Default return     
-                return true;
+            } catch (Exception $e) {
 
-            } catch(Exception $e) {
-
-                // Return an error
+                // Return false to stop the execution
                 return false;
 
             }
 
+            // Default return
+            return true;
+        }
+
+        /**
+         * Generatea error message to the user
+         */
+        private function error() {
+
+            print('
+            <div class="playlist playlist--error">
+                <div class="sa">
+                    <div class="sa-error">
+                        <div class="sa-error-x">
+                            <div class="sa-error-left"></div>
+                            <div class="sa-error-right"></div>
+                        </div>
+                        <div class="sa-error-placeholder"></div>
+                        <div class="sa-error-fix"></div>
+                    </div>
+                </div>
+            </div>
+
+            <h3>Er is geen playlist gevonden</h3>
+            <p>Voor de geselecteerde dag, konden geen songs ophehaald worden.</p>
+            ');
+
+            return;
+        }
+
+        /**
+         * Show a header where the user can navigate to a new date
+         */
+        private function showHeader() {
+
+            // First Create the correct dates that needs to be handled
+            $this->settings["selectedDay"] = ($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+            $this->settings["nextDay"] = date('Y-m-d', strtotime('+1 day', strtotime($this->settings["selectedDay"])));
+
+            // Setup the formatter
+            $fmt = datefmt_create('nl_BE', IntlDateFormatter::FULL, IntlDateFormatter::FULL, 'Europe/Brussels', IntlDateFormatter::GREGORIAN, 'EEEE dd MMMM yyyy');
+
+            // Start the header
+            print('
+            <div class="playlistHeader">
+                <a href="./?date=' . date('Y-m-d', strtotime('- 1 days', strtotime($this->settings['selectedDay']))) . '"><i class="fas fa-angle-left previousBtn"></i></a>
+                <div>' . $fmt->format(datetime::createfromformat('Y-m-d', $this->settings["selectedDay"])) . '</div>
+            ');
+
+            // Check if there is a next date based
+            if($this->settings["nextDay"] > date('Y-m-d'))
+                print('<a href="#"><i class="fas fa-angle-right disabled"></i></a>');
+            else
+                print('<a href="./?date=' . $this->settings["nextDay"] . '"><i class="fas fa-angle-right nextBtn"></i></a>');
+
+            // Close the header
+            print('</div>');
+            
 
         }
-        
+
         /**
-         * Print the songs from a specified array
+         * List all the songs
          */
-        private function printSongs($songs) {
-            // Loop trough songs
-            foreach($songs as $song) {
-                // Create element
+        private function listSongs() {
+
+            $fmt = datefmt_create('nl_BE', IntlDateFormatter::FULL, IntlDateFormatter::FULL, 'Europe/Brussels', IntlDateFormatter::GREGORIAN, 'HH:mm');
+
+            // First, generate the playlist holder
+            print('<div class="playlist">');
+
+            // Loop over the songs
+            foreach($this->settings['songs'] as $song) {
+                // Do some other stuff here
                 print('
-                <div class="songItem">
-                    <div class="songItem--time">
-                        ' . date('H:i', strtotime($song['start'])) . '
+                <div class="playlist--item">
+                    <div class="playlist--item__timestamp">
+                        ' . $fmt->format(datetime::createfromformat('Y-m-d H:i:s', $song['start'])) . '
                     </div>
-                    <div class="songItem--cover">
+                    <div class="playlist--item__cover">
                         <img src="data:image/png;charset=utf-8;base64,' . $song['cover'] . '">
                     </div>
-                    <div class="songItem--content">
-                        <div>
-                            <p class="artist">' . $song['artist'] . '</p>
-                            <p class="title">' . $song['title'] . '</p>
-                        </div>
+                    <div class="playlist--item__info">
+                        <p><span>' . $song['artist'] . '</span></p>
+                        <p>' . $song['title'] . '</p>
                     </div>
                 </div>
                 ');
             }
-        }
 
-        /**
-         * Print an error when needed
-         */
-        private function printError() {
-            print('
-                <div class="errorBox">
-                    <h2>Uhoh</h2>
-                    <p>Het lijkt erop dat het even niet mogelijk is om de songs op te halen. Probeer later opnieuw</p>
-                </div>
-            </div>
-            ');
+            // At last, close the playlist holder
+            print('</div>');
         }
-
     }
 }
